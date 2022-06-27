c0f0469c94f32db5689482c8d5be14ea5d3585ab
SouthTea <37215562+SouthTea@users.noreply.github.com>
2022-06-27 13:29:25 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,84 @@
+<script>
+	export default {
+		globalData: {
+			topiccard:[]
+		},
+		onLaunch: function() {
+			console.log('App Launch')
+
+		},
+		onShow: function() {
+			console.log('App Show')
+
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
+		methods: {}
+	}
+</script>
+
+<style lang="scss">
+	/*每个页面公共css */
+	/* @import '@/uni_modules/uni-scss'; */
+	$main_top:60upx;
+	$headimg_h:120upx;
+	view{
+		font-family: -apple-system-font, BlinkMacSystemFont, "Helvetica Neue", "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei UI", "Microsoft YaHei", Arial, sans-serif;
+	}
+	.title {
+		margin-top: $main_top;
+		height: 150upx;
+		font-size: 40upx;
+		display: flex;
+		align-items: center;
+	}
+
+	.main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		//justify-content: center;
+
+		background-image: linear-gradient(135deg, #fff, #eee, #ccc);
+		// background-color: rgba($color: #ccc, $alpha: .1);
+		height: 100vh;
+
+	}
+
+	@keyframes danru {
+		from {
+			opacity: 0;
+			top: -20upx;
+		}
+
+		to {
+			opacity: 1;
+			top: 0upx;
+		}
+	}
+@keyframes danru2 {
+		from {
+			opacity: 0;
+			margin-bottom: 40upx;
+		}
+
+		to {
+			opacity: 1;
+			margin-bottom: 20upx;
+		}
+	}
+	.card {
+		animation: danru .5s linear forwards;
+		border-radius: 40upx;
+		// box-shadow: -200upx -5upx 50upx 0 rgba($color: #eee, $alpha: 0.5), 300upx -5upx 50upx 0 rgba($color: #c8eaf8, $alpha: 0.8);
+		box-shadow: 0 0 50upx 0 rgba($color: #ccc, $alpha: .3);
+		background-color: #fff;
+		width: 700upx;
+		margin: 20upx 0;
+		position: relative;
+		opacity: 0;
+		padding: 20upx;
+		box-sizing: border-box;
+	}
+</style>
---
+++ README.md
@@ -0,0 +1,46 @@
+# uni-app-WeChat-DoExam
+
+A do questions  mini program for WeChat based on Uni-App
+
+一个基于uni-app开发的简易微信刷题小程序
+
+本程序开源于[Github](https://github.com/SouthTea/uni-app-WeChat-DoExam)
+
+仅前端部分（含有基本信息、抽题面板、答题记录-右滑动删除效果、题目收藏、答题界面及题卡-已答|未答|错误三种状态）
+
+-  个人信息
+  
+  * 题库数量
+  
+  * 抽题记录
+  
+  * 刷题指数
+  
+  * 排行
+
+- 抽题面板
+  
+  * 题库列表
+  * 抽题数量
+
+- 抽题记录
+  
+  * 题库标题
+  * 创建时间
+  * 答卷类型标签
+  * 答题进度条
+  * 分数
+
+- ...
+
+预览，请用微信扫码：
+
+![](E:\文档\21移动互联2\项目\刷题\preview\er.jpg)
+
+图片预览：
+
+![Exam](.\preview\2.png)
+
+![Exam2](.\preview\3.png)
+
+![Collext](.\preview\4.png)
